03ca67dde65e9e1c8f94988b7d627cd78348820e
Tobias Lust <tobiaslust.dev@gmail.com>
2023-12-05 15:37:38 -0300

    finish scripts js and start css

--- app.js
+++ app.js
@@ -1,68 +1,97 @@
-const HANDS = ["rock", "paper", "scissors"];
+// Selección de elementos del DOM
+const btnPlayer = document.querySelectorAll("#playerSelection button");
+const btnCpu = document.querySelector("#cpuBtn");
+const round = document.querySelector("#round span");
+const divScore = document.querySelector("#score");
+const detailRound = document.querySelector("#detailRound");
+const playerScore = document.querySelector("#playerScore span");
+const cpuScore = document.querySelector("#cpuScore span");
 
-function getComputerChoice() {
+// Asignación de eventos a los botones del jugador
+btnPlayer.forEach((btn) => btn.addEventListener("click", startGame));
+
+// Función para comenzar el juego
+function startGame(e) {
+  const playerChoice = e.target.textContent;
+  const computerChoice = getComputerSelection();
+  btnCpu.textContent = computerChoice;
+  playRound(playerChoice, computerChoice);
+}
+
+// Función para obtener la selección aleatoria de la computadora
+function getComputerSelection() {
+  const HANDS = ["rock", "paper", "scissors"];
   return HANDS[Math.floor(Math.random() * HANDS.length)];
 }
 
-function getPlayerSelection() {
-  let select;
-  do {
-    select = prompt("Choice rock,paper,scissors").trim().toLowerCase();
-  } while (!HANDS.includes(select));
-  return select;
-}
-
-function displayRoundResult(result, playerSelection, computerSelection) {
-  if (result === "win") {
-    console.log(`¡Win! ${playerSelection} beats ${computerSelection}`);
-  } else if (result === "lose") {
-    console.log(`¡Lose! ${computerSelection} beats ${playerSelection}`);
-  } else {
-    console.log("Tie");
-  }
-}
-
-function playRound() {
-  const playerSelection = getPlayerSelection();
-  const computerSelection = getComputerChoice();
-
-  if (
-    (playerSelection === HANDS[0] && computerSelection === HANDS[2]) ||
-    (playerSelection === HANDS[1] && computerSelection === HANDS[0]) ||
-    (playerSelection === HANDS[2] && computerSelection === HANDS[1])
-  ) {
-    displayRoundResult("win", playerSelection, computerSelection);
-    return "win";
-  } else if (playerSelection === computerSelection) {
+// Función principal para jugar una ronda
+function playRound(playerChoice, computerChoice) {
+  if (isWin(playerChoice, computerChoice)) {
+    displayRoundResult("win", playerChoice, computerChoice);
+    playerScore.textContent++;
+  } else if (playerChoice === computerChoice) {
     displayRoundResult("tie");
-    return "tie";
   } else {
-    displayRoundResult("lose", playerSelection, computerSelection);
-    return "lose";
+    displayRoundResult("lose", playerChoice, computerChoice);
+    cpuScore.textContent++;
+  }
+  round.textContent++;
+  endGame(playerScore, cpuScore);
+}
+
+// Función para verificar si el jugador gana la ronda
+function isWin(playerChoice, computerChoice) {
+  return (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  );
+}
+
+// Función para mostrar el resultado de la ronda
+function displayRoundResult(result, playerSelection, computerSelection) {
+  switch (result) {
+    case "win":
+      detailRound.textContent = `¡Win! ${playerSelection} beats ${computerSelection}`;
+      break;
+    case "lose":
+      detailRound.textContent = `¡Lose! ${computerSelection} beats ${playerSelection}`;
+      break;
+    default:
+      detailRound.textContent = "Tie";
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let cpuScore = 0;
-
-  for (let round = 1; round <= 5; round++) {
-    console.log(`ROUND: ${round}`);
-    const result = playRound();
-
-    if (result === "win") {
-      playerScore++;
-    } else if (result === "lose") {
-      cpuScore++;
-    } else {
-      round--;
-    }
-
-    console.log(`PLAYER: ${playerScore} CPU: ${cpuScore} `);
+// Función para manejar el final del juego
+function endGame(playerScore, cpuScore) {
+  if (playerScore.textContent == 5 || cpuScore.textContent == 5) {
+    const resultMessage =
+      playerScore.textContent == 5 ? "You win the game!" : "You lose the game!";
+    detailRound.textContent = resultMessage;
+    btnPlayer.forEach((btn) => (btn.disabled = true));
+    restartBtn();
   }
-
-  console.log("Finish game! Result:");
-  console.log(`PLAYER: ${playerScore} CPU: ${cpuScore}`);
 }
 
-game();
+// Función para reiniciar el juego
+function restartGame() {
+  playerScore.textContent = "0";
+  cpuScore.textContent = "0";
+  round.textContent = "0";
+  detailRound.textContent = "";
+  btnCpu.textContent = "?";
+  btnPlayer.forEach((btn) => (btn.disabled = false));
+  const btnRestart = document.querySelector("#restartBtn");
+  if (btnRestart) {
+    btnRestart.remove();
+  }
+}
+
+// Función para agregar el botón de reinicio
+function restartBtn() {
+  const btnRestart = document.createElement("button");
+  btnRestart.id = "restartBtn";
+  btnRestart.textContent = "Restart";
+  btnRestart.addEventListener("click", () => restartGame());
+  divScore.appendChild(btnRestart);
+}
